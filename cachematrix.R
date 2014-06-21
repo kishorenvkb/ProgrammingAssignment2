882da02eaff8439724a5c8c6af7e1295e78829b7
Kishore <kishorenvkb@gmail.com>
2014-06-20 22:29:08 -0400

    Updated the code to inverse the matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This functions calculates the inverse of the matrix
+## If the inverse was already calculated once, it reports the results
+## from cache instead of performing the inverse again on the matrix
 
-## Write a short comment describing this function
+## Perform Smart Inverse on a Matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv  <- NULL
+        set  <- function(y){
+                x <<- y
+                inv <<- NULL 
+        }
+        get  <- function() x
+        setinverse  <- function(inverse) inv  <<- inverse
+        getinverse  <- function() inv
+        list(set= set, get = get, 
+             setinverse = setinverse, 
+             getinverse = getinverse)
 }
 
 
@@ -12,4 +23,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv  <- x$getinverse()
+        if (!is.null(inv)){
+                message("getting cached data")
+                return(inv)
+        }
+        data  <- x$get()
+        inv  <- solve(data, ...) ## inverses the matrix
+        x$setinverse(inv)
+        inv
+
 }
